fc33cc434da90173d98158efce9e23605b63df41
hiremekr <hireme.kr@gmail.com>
2026-07-06 13:39:58 +0900

    Update CalculatorPage.tsx

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1297,11 +1297,11 @@ export const CalculatorPage = () => {
         <a href="/company/talents" class="btn-cta-primary">
           검증된 외국인 인재 살펴보기 →
         </a>
-        <a href="http://pf.kakao.com/_pPxnFxj/chat" target="_blank" class="btn-kakao">
-          <svg viewBox="0 0 24 24" fill="#FEE500" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 3C6.48 3 2 6.58 2 11c0 2.83 1.86 5.31 4.65 6.74-.2.74-.73 2.7-.83 3.12-.13.52.19.51.4.37.16-.11 2.61-1.78 3.66-2.5.71.1 1.43.16 2.12.16 5.52 0 10-3.58 10-8s-4.48-8-10-8z"/>
+        <a href="tel:16601345" class="btn-kakao btn-tel">
+          <svg viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.13.96.36 1.9.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.9.34 1.85.57 2.81.7A2 2 0 0 1 22 16.92z"/>
           </svg>
-          카카오톡 1:1 무료 상담
+          대표번호 1660-1345 상담
         </a>
       </div>
     </div>
